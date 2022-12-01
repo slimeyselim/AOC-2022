1755f242f8c9dcadc1269ea386777a82c7d062b0
Metin Selim <metinjselim@hotmail.co.uk>
2022-12-01 06:41:57 +0000

    day 1 Signed-off-by: Metin Selim <metinjselim@hotmail.co.uk>

---
+++ 001.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+const fileName = 'input/001.txt';
+let fileContent = fs.readFileSync(fileName, 'utf8');
+const elves = fileContent.split(/\n\s*\n/);
+console.log(elves.length);
+let maxCalCount = 0;
+
+//PART ONE
+
+elves.map(elf => {
+    let calories = elf.split("\n");
+    let elfCalCount = calories.reduce((a,b) => {return +a + +b},0);
+    if(elfCalCount > maxCalCount) maxCalCount = elfCalCount;
+})
+
+console.log("MAX CALS", maxCalCount);
+
+//PART TWO
+let elvesCals: number[] = [];
+
+elves.map(elf => {
+    let calories = elf.split("\n");
+    let elfCalCount = calories.reduce((a,b) => {return +a + +b},0);
+    elvesCals.push(elfCalCount);
+})
+
+elvesCals.sort((a,b) => b-a);
+
+let top3Elves = elvesCals.slice(0,3).reduce((a,b) => {return +a + +b},0);
+
+console.log("TOP 3 ELVES ",top3Elves);
+
+//bash clear console - reset
